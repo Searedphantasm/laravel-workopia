1cefb68521d92f0a925e20aeafb131d262508951
Searedphantasm <searedphantasm@gmail.com>
2025-01-10 11:16:31 -0800

    finishing basic components

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -12,11 +12,16 @@
             crossorigin="anonymous"
             referrerpolicy="no-referrer"
     />
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}" />
     @vite("resources/css/app.css")
     <title>{{$title ?? 'Workopia | Find and list Jobs'}}</title>
 </head>
     <body class="bg-gray-100">
         <x-header />
+        @if(request()->is('/'))
+            <x-hero />
+            <x-top-banner />
+        @endif
         <main class="container mx-auto p-4 mt-4">
             {{$slot}}
         </main>
